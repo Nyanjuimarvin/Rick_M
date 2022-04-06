c4a2aa064cd2804329139ef7a2ed0786c9c9af75
marvin <marnyanjui@gmail.com>
2022-04-06 13:50:12 +0300

    Add required route

--- app/src/main/java/com/nyanjuimarvin/firebaserickm/api/RickMortyApi.kt
+++ app/src/main/java/com/nyanjuimarvin/firebaserickm/api/RickMortyApi.kt
@@ -1,4 +1,10 @@
 package com.nyanjuimarvin.firebaserickm.api
 
+import com.nyanjuimarvin.firebaserickm.models.RickMortyResponse
+import retrofit2.Call
+import retrofit2.http.GET
+
 interface RickMortyApi {
+    @GET("character")
+    fun getCharacter(): Call<RickMortyResponse>
 }
