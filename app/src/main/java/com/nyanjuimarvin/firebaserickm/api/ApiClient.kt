40c1cac2a1a63e8d0cd64f0d3f8246a42542e43d
marvin <marnyanjui@gmail.com>
2022-04-06 14:30:23 +0300

    Add retrofit client

--- app/src/main/java/com/nyanjuimarvin/firebaserickm/api/ApiClient.kt
+++ app/src/main/java/com/nyanjuimarvin/firebaserickm/api/ApiClient.kt
@@ -1,4 +1,30 @@
 package com.nyanjuimarvin.firebaserickm.api
 
-class ApiClient {
+import com.nyanjuimarvin.firebaserickm.constants.Constants.Companion.BASE_URL
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiClient {
+    fun getClient():RickMortyApi{
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val myRequest = chain.request().newBuilder()
+                    .build()
+                chain.proceed(myRequest)
+            }
+            .build()
+
+
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(
+            GsonConverterFactory.create())
+            .build();
+
+        return retrofit.create(RickMortyApi::class.java);
+    }
+
 }
